67f32205221ba4cbc7518df7c61d825acd52211f
Helena S. Ágústsdóttir <helenaa19@ru.is>
2020-09-03 14:19:57 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -1,7 +1,9 @@
 # Forrit sem finnur hæsta gildið sem notandi slær inn.
-num_int = int(input("Input a number: "))
 # Spyrja notanda um jákvætt gildi.
+num_int = int(input("Input a number: "))
 # Setja það sem hæsta gildið.
 # Spyrja notanda um annað jákvætt gildi
 # Bera saman gildi notenda og finna hæsta gildið.
+# Prenta út hæsta gildið.
+print("The maximum is", max_int) 
 # Ef notandi setur inn neikvæða tölu, skal stoppa forritið.
